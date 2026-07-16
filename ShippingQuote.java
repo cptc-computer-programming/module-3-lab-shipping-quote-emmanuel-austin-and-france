74bffb09bc75ddbbda9bdc52afc7eadc1abdd8bc
Austin Goss <austindgoss.dev@gmail.com>
2026-07-16 11:06:08 -0700

    formatting changes

--- ShippingQuote.java
+++ ShippingQuote.java
@@ -1,6 +1,6 @@
 public class ShippingQuote {
     public static void main(String[] args) {
-                // Package definition (example values — you can change these)
+        // Package definition 
         double weight = 12.5;   // pounds
         double length = 20.0;   // inches
         double width  = 15.0;   // inches
